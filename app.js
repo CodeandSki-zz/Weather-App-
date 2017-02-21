6dae479f0a24d0711a6fa1b867d8c0783c53a85d
CodeandSki <a.k.hove@gmail.com>
2017-02-20 18:01:20 -0700

    First commit. Existing repository for weather app

---
+++ app.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+    var geoCord = "";
+    navigator.geolocation.getCurrentPosition(function(position) {
+        geoCord = (position.coords.latitude, position.coords.longitude);
+        $(".h1city").html(geoCord);
+    });
+    console.log(geoCord);
+
+
+});
+
+
+
+var str = 'F';
+function ajaxCall() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon});
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+    console.log(response);
+}
+
+//Change Between Celcius and Farenheight and Claculate new Temperature
+function celciusChange() {
+
+    var celcius = document.getElementById('celciusButton');
+    var temperature = "";
+    if (celcius.innerHTML == 'Fahrenheit') {
+        var temperature = 'far';
+        console.log(temperature);
+        celcius.innerHTML = ('Celcius');
+    }
+    else {
+        var temperature = 'cel';
+        console.log(temperature);
+        celcius.innerHTML = ('Fahrenheit');
+    }
+}
