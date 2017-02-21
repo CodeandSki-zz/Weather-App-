0da48c6c46a76989c59c8cca996f6252bbb2e920
CodeandSki <a.k.hove@gmail.com>
2017-02-20 18:58:49 -0700

    fixed celcius button event listener

--- app.js
+++ app.js
@@ -7,20 +7,19 @@ $( document ).ready(function() {
     alert(geoCord);
 
 
-    var FarenheightButton = document.getElementById('celciusButton');
-    FarenheightButton.addEventListener("click", cleciusChange, false)
+
     // target.addEventListener(type, listener[, options]);
 });
 
-var str = 'F';
-function ajaxCall() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon});
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
-    console.log(response);
-}
+// var str = 'F';
+// function ajaxCall() {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.open("POST", api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon});
+//     xhttp.setRequestHeader("Content-type", "application/json");
+//     xhttp.send();
+//     var response = JSON.parse(xhttp.responseText);
+//     console.log(response);
+// }
 
 //Change Between Celcius and Farenheight and Claculate new Temperature
 function celciusChange() {
@@ -37,4 +36,8 @@ function celciusChange() {
         console.log(temperature);
         celcius.innerHTML = ('Fahrenheit');
     }
+}
+window.onload=function(){
+    var FarenheightButton = document.getElementById('celciusButton');
+    FarenheightButton.addEventListener("click", celciusChange, false)
 }
