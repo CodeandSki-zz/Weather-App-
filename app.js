0b48cca52b5acee9883ff8d3638991bcdfb3b713
CodeandSki <a.k.hove@gmail.com>
2017-02-26 13:20:18 -0700

    added functions to jquery onload function and solved console undefined issue

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ $( document ).ready(function() {
     var FarenheightButton = document.getElementById('celciusButton');
     FarenheightButton.addEventListener("click", celciusChange, false);
 
-    var geoCord = "";
-    // navigator.geolocation.getCurrentPosition(function(position) {
-    //     geoCord = (position.coords.latitude, position.coords.longitude);
-    //     $(".h1city").html(geoCord);
-    // });
-    // alert(geoCord);
+    //Change Between Celcius and Farenheight and Claculate new Temperature
+    function celciusChange() {
 
+        var celcius = document.getElementById('celciusButton');
+        var temperature = "";
+        if (celcius.innerHTML == 'Fahrenheit') {
+            var temperature = 'far';
+            celcius.innerHTML = ('Celcius');
+        }
+        else {
+            var temperature = 'cel';
+            celcius.innerHTML = ('Fahrenheit');
+        }
+        var testtt = navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+
+
+    //Successful geolocation//
+     function success(pos) {
+      var crd = pos.coords;
+      console.log('Your current position is:');
+      console.log(crd.latitude);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+
+      var latitude = crd.latitude;
+      var longitude = crd.longitude;
+      // console.log(test);
+    };
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
 });
 
  var latitude = '';
@@ -23,40 +49,8 @@ $( document ).ready(function() {
 
 var latitude="";
 var longitude="";
-//Successful geolocation//
- function success(pos) {
-  var crd = pos.coords;
-  console.log('Your current position is:');
-  console.log(crd.latitude);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
 
-  var latitude = crd.latitude;
-  var longitude = crd.longitude;
-  // console.log(test);
-};
 
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-};
-
-//Change Between Celcius and Farenheight and Claculate new Temperature
-function celciusChange() {
-
-    var celcius = document.getElementById('celciusButton');
-    var temperature = "";
-    if (celcius.innerHTML == 'Fahrenheit') {
-        var temperature = 'far';
-        celcius.innerHTML = ('Celcius');
-    }
-    else {
-        var temperature = 'cel';
-        celcius.innerHTML = ('Fahrenheit');
-    }
-
-    var testtt = navigator.geolocation.getCurrentPosition(success, error, options);
-    //Print results to console
-    console.log(testtt);
 
 
 
@@ -70,7 +64,13 @@ function celciusChange() {
     //
     // }
 
-}
+
+
+
+
+
+
+
 
 //Onload function//
 // window.onload=function(){
