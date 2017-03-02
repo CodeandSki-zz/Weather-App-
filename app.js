48b5c72d7eb312e6e39f6359ec17a5aec8a25859
CodeandSki <a.k.hove@gmail.com>
2017-03-02 08:47:21 -0700

    removed console log of api call json response

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ $( document ).ready(function() {
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send();
         var response = JSON.parse(xhttp.responseText);
-        console.log(response);
         
+
     }
 
 
